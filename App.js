64e3e821bc956b9883f0d2821b4b87779197cf28
yyeonkim <seyeon.dev@gmail.com>
2022-11-20 14:38:45 +0900

    Uninstall Recoil

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import MyNotesScreen from "./src/screens/myNotes";
 
 const Stack = createNativeStackNavigator();
